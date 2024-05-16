f071d47ee8292fbbd3d7c72f33673f0c839214d4
Jessica Blanton <jmblanton@gmail.com>
2024-05-16 18:08:02 -0500

    Update ptree_submit.sh

--- ptree_submit.sh
+++ ptree_submit.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=0-00:10:00   ### Wall clock time limit in Days-HH:MM:SS
 #SBATCH --nodes=1   ### Number of nodes to use
 #SBATCH --ntasks-per-node=1   ### Number of tasks to run per node
-#SBATCH --cpus-per-task=20  ### Number of cpus to use
+#SBATCH --cpus-per-task=20  ### Number of cpus available for task
 
 # Make sure muscle 5.1.linux64 is available
 module load muscle/5.1
@@ -19,7 +19,7 @@ module load qiime2/2018.2
 mkdir primate_tree
 
 # Align sequences
-muscle -align mammal_data/primate_cytb.fna -output primate_tree/aln.fna -threads 2 -log primate_tree/muscle_aln.log ;
+muscle -align mammal_data/primate_cytb.fna -output primate_tree/aln.fna -threads 1 -log primate_tree/muscle_aln.log
 
-# FastTree version 2.1.10
+# Build tree
 FastTree -log primate_tree/fasttree.log -nt primate_tree/aln.fna > primate_tree/p_tree.nwk
